5f94c5e6d99b1ae5c4299fb99cf9cd352a6ab135
cosimini <davide.scalcon@studenti.unipd.it>
2024-04-22 21:13:16 +0000

    Grounded EN pin, moved a mounting hole

---
+++ controllers_code/include/pinout.h
@@ -0,0 +1,18 @@
+/*
+* Define pinout
+*/
+
+#define ROW0 16
+#define ROW1 15
+#define ROW2 14
+#define ROW3 13
+
+#define COL0 12
+#define COL1 6
+#define COL2 7
+#define COL3 8
+#define COL4 9
+#define COL5 10
+#define COL6 11
+
+#define EN_PIN 2
--- controllers_code/src/main.cpp
+++ controllers_code/src/main.cpp
@@ -1,13 +1,31 @@
+#define DEBUG_MODE 1
+
 #include "Arduino.h"
 #include "Keyboard.h"
+// project specific libraries in ../include
+#include "pinout.h"
 
 void setup() {
   // Keyboard.begin();
   // delay(1000);
-  Serial.begin(9600);
-  Serial1.begin(9600);
+  Serial1.begin(9600);  // Serial1 is the hardware UART on pins 1 and 2 of the pro-micro
   // Set up pins
-  pinMode(2, INPUT_PULLUP);
+  pinMode(ROW0, OUTPUT);
+  pinMode(ROW1, OUTPUT);
+  pinMode(ROW2, OUTPUT);
+  pinMode(ROW3, OUTPUT);
+  pinMode(COL0, INPUT_PULLUP);
+  pinMode(COL1, INPUT_PULLUP);
+  pinMode(COL2, INPUT_PULLUP);
+  pinMode(COL3, INPUT_PULLUP);
+  pinMode(COL4, INPUT_PULLUP);
+  pinMode(COL5, INPUT_PULLUP);
+  pinMode(COL6, INPUT_PULLUP);
+  pinMode(EN_PIN, INPUT_PULLUP);
+  // Debug enabled stuff
+  #if DEBUG_MODE == 1
+  Serial.begin(9600);
+  #endif
 }
 
 void loop() {
