e149e09ceb73da484bb2cd58416a8958065b7035
cosimini <davide.scalcon@studenti.unipd.it>
2024-06-03 19:05:41 +0000

    Test the UART communication

--- pullup_test_code/src/main.cpp
+++ pullup_test_code/src/main.cpp
@@ -5,35 +5,50 @@ bool status[nport];
 
 void setup() {
 	Serial.begin(9600);
-	pinMode(10, OUTPUT);
-	pinMode(14, OUTPUT);
-	pinMode(15, OUTPUT);
-	pinMode(16, OUTPUT);
-	for(int k = 2; k <= 9; k++) pinMode(k, INPUT_PULLUP);
-	for(int k = 0; k < nport; k++) status[k] = false;
+	// pinMode(10, OUTPUT);
+	// pinMode(14, OUTPUT);
+	// pinMode(15, OUTPUT);
+	// pinMode(16, OUTPUT);
+	// digitalWrite(10, LOW);
+	// digitalWrite(14, LOW);
+	// digitalWrite(15, LOW);
+	// digitalWrite(16, LOW);
+	// for(int k = 2; k <= 9; k++) pinMode(k, INPUT_PULLUP);
+	// for(int k = 0; k < nport; k++) status[k] = false;
+	Serial1.begin(115200);
 }
 
 void loop() {
-	digitalWrite(10, HIGH);
-	digitalWrite(14, HIGH);
-	digitalWrite(15, HIGH);
-	digitalWrite(16, HIGH);
-	for(int k = 2; k <= 9; k++) {
-		if(digitalRead(k) && !status[k-2]) {
-			Serial.print(k, DEC);
-			Serial.print(" up");
-			Serial.println();
+	Serial1.write('s');
+	Serial1.write('m');
+	Serial1.write((char) 0);
+	// delay(100);
+	while(Serial1.available()) {
+		char data = (char) Serial1.read();
+		if(data != 0 && data != 'k' &data != 'm') {
+			Serial.print(data);
 		}
-		if(!digitalRead(k) && status[k-2]) {
-			Serial.print(k, DEC);
-			Serial.print(" down");
-			Serial.println();
-		}
-		status[k-2] = digitalRead(k);
 	}
-	digitalWrite(10, LOW);
-	digitalWrite(14, LOW);
-	digitalWrite(15, LOW);
-	digitalWrite(16, LOW);
-	delay(25);
+	// digitalWrite(10, HIGH);
+	// digitalWrite(14, HIGH);
+	// digitalWrite(15, HIGH);
+	// digitalWrite(16, HIGH);
+	// for(int k = 2; k <= 9; k++) {
+	// 	if(digitalRead(k) && !status[k-2]) {
+	// 		Serial.print(k, DEC);
+	// 		Serial.print(" up");
+	// 		Serial.println();
+	// 	}
+	// 	if(!digitalRead(k) && status[k-2]) {
+	// 		Serial.print(k, DEC);
+	// 		Serial.print(" down");
+	// 		Serial.println();
+	// 	}
+	// 	status[k-2] = digitalRead(k);
+	// }
+	// digitalWrite(10, LOW);
+	// digitalWrite(14, LOW);
+	// digitalWrite(15, LOW);
+	// digitalWrite(16, LOW);
+	delay(50);
 }
