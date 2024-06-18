e1ef341eac048dac96484e48e969b53fd7d36a7e
cosimini <davide.scalcon@studenti.unipd.it>
2024-06-18 20:58:51 +0000

    Move to simple 2 layers layout

--- controllers_code/include/keymaps.h
+++ controllers_code/include/keymaps.h
@@ -32,28 +32,34 @@ char cHOME = KEY_HOME;
 char cEND = KEY_END;
 char cPAGEDOWN = KEY_PAGE_DOWN;
 char cPAGEUP = KEY_UP_ARROW;
+char cF1 = KEY_F1;
+char cF2 = KEY_F2;
+char cF3 = KEY_F3;
+char cF4 = KEY_F4;
+char cF5 = KEY_F5;
+char cF6 = KEY_F6;
+char cF7 = KEY_F7;
+char cF8 = KEY_F8;
+char cF9 = KEY_F9;
+char cF10 = KEY_F10;
+char cF11 = KEY_F11;
+char cF12 = KEY_F12;
 
 #if SIDE == 1	// right side
-const int nModifiers = 2;
-int modifiers[2][nModifiers] = {{1, 2}, {0, 0}}; 	// Address of the modifiers
+const int nModifiers = 1;
+int modifiers[2][nModifiers] = {{2}, {0}};  // Address of the modifiers
 char keyMap[N_LAYERS][N_ROWS][N_COLS] = {	// Keymap for each possible layer
 	{	// Layer 0
 		{'=',  cBS,    '0', '9', '8', '7', '6'},
-		{0,    '/',    'p', 'o', 'i', 'u', 'y'},
+		{cESC,  '/',    'p', 'o', 'i', 'u', 'y'},
 		{0,    cENTER, ';', 'l', 'k', 'j', 'h'},
 		{'\\', '\'',   '-', '.', ',', 'm', 'n'}
 	},
 	{	// Layer 1
-		{'=', cDEL,   '0',  '9',    '8',     '7',       '6'},
-		{0,   '/',    ']',  '[',    '}',     '{',       '#'},
-		{0,   cENTER, '\\', cRIGHT, cUP,     cDOWN,     cLEFT},
-		{0,   '\'',   '-',  cEND,   cPAGEUP, cPAGEDOWN, cHOME}
-	},
-	{	// Layer 2
-		{'=', cBS,    '0', '9', '8', '7', '6'},
-		{0,   '/',    'p', 'o', 'i', 'u', 'y'},
-		{0,   cENTER, ';', 'l', 'k', 'j', 'h'},
-		{0,   '\'',   '-', '.', ',', 'm', 'n'}
+		{cF11, cDEL,   cF10, cF9,    cF8,     cF7,       cF6},
+		{0,    '/',    ']',  '[',    '}',     '{',       '#'},
+		{0,    cENTER, '\\', cRIGHT, cUP,     cDOWN,     cLEFT},
+		{0,    '\'',   '-',  cEND,   cPAGEUP, cPAGEDOWN, cHOME}
 	}
 };
 
@@ -68,17 +74,11 @@ char keyMap[N_LAYERS][N_ROWS][N_COLS] = {	// Keymap for each possible layer
 		{cWIN, cCTRL,  'z', 'x', 'c', 'v', 'b'}
 	},
 	{	// Layer 1
-		{'`',  cESC,   '1', '2', '3', '4', '5'},
-		{' ',  cTAB,   'e', '7', '8', '9', 'e'},
-		{cALT, cSHIFT, '.', '4', '5', '6', '.'},
-		{cWIN, cCTRL,  '0', '1', '2', '3', '0'}
+		{cF12, cESC,   cF1, cF2, cF3, cF4, cF5},
+		{0,    cTAB,   0,   '7', '8', '9', 'e'},
+		{cALT, cSHIFT, 0,   '4', '5', '6', '.'},
+		{cWIN, cCTRL,  0,   '1', '2', '3', '0'}
 	},
-	{	// Layer 2
-		{'`',  cESC,   '1', '2', '3', '4', '5'},
-		{' ',  cTAB,   'q', 'w', 'e', 'r', 't'},
-		{cALT, cSHIFT, 'a', 's', 'd', 'f', 'g'},
-		{cWIN, cCTRL,  'z', 'x', 'c', 'v', 'b'}
-	}
 };
 
 #endif
